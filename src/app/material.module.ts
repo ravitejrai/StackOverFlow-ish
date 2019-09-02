da545b483ef6a4a10a8bad4803cab8ba2d462c00
manogna <manogna.madineni6@gmail.com>
2019-09-02 10:23:37 -0400

    sfd

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from  '@angular/core';
  
-//import {MatButtonModule,MatToolbarModule} from  '@angular/material';
 import {MatNativeDateModule,MatDatepickerModule,MatIconModule,
     MatButtonModule,MatCheckboxModule, MatToolbarModule, MatCardModule,
     MatFormFieldModule,MatInputModule,MatRadioModule,MatListModule} from  '@angular/material';
-//import {MatDatepickerModule} from  '@angular/material/datepicker';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
  
  
 @NgModule({
