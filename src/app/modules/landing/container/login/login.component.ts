8398f1e4cf0dffde544adb7b8bf02fcc971562c1
manogna <manogna.madineni6@gmail.com>
2019-09-02 10:25:31 -0400

    comments

--- src/app/modules/landing/container/login/login.component.ts
+++ src/app/modules/landing/container/login/login.component.ts
@@ -20,29 +20,29 @@ export class LoginComponent implements OnInit {
 
   login():void{
     this.loginPageService.getUserDetails(this.email, this.password).subscribe((data : any) => { 
-      console.log(data, "service response");
-      console.log(data.email1,"inside post");
-      console.log(data.password2);
-      console.log(data.id);
+      //console.log(data, "service response");
+      //console.log(data.email1,"inside post");
+      //console.log(data.password2);
+      //console.log(data.id);
 
       if(data.id == 4) {
-        console.log(data.email1,"inside if");
+        //console.log(data.email1,"inside if");
         this.router.navigate(['/dashboard']);
       }else {
         alert("Invalid credentials");
       }
     },error =>{
-      console.log(error,'inside ......');
+      //console.log(error,'inside ......');
       this.handleError(error);
     } )
   }
   private handleError(errorResponse : HttpErrorResponse) {
     //client side or server error
     if(errorResponse.error instanceof ErrorEvent){
-      console.error("client side error",errorResponse.error.message);
+      //console.error("client side error",errorResponse.error.message);
       alert("client side error,please try again");
     }else {
-      console.error("Server side error",errorResponse);
+      //console.error("Server side error",errorResponse);
       alert("server side error,please try again");
     }
     return throwError('there is problem with service');
