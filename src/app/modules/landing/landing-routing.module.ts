78e42332c3c2c763aaad97a2f52125ef74afa799
jay pathak <jay@officeintegrator.com>
2019-09-03 07:13:32 -0700

    rx js learning

--- src/app/modules/landing/landing-routing.module.ts
+++ src/app/modules/landing/landing-routing.module.ts
@@ -4,6 +4,7 @@ import { LayoutComponent } from './container/layout/layout.component';
 import { RegistrationComponent } from './container/registration/registration.component';
 import { LoginComponent } from './container/login/login.component';
 import { HomeComponent } from './container/home/home.component';
+import { LearnrxjsComponent } from './container/learnrxjs/learnrxjs.component';
 
 
 export const routes: Routes = [
@@ -26,6 +27,10 @@ export const routes: Routes = [
         path: 'login',
         component: LoginComponent,
 
+      },
+      {
+        path: 'learnrxjs',
+        component: LearnrxjsComponent,
       }
     ]
   }
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -6,10 +6,11 @@ import { LayoutComponent } from './container/layout/layout.component';
 import { RegistrationComponent } from './container/registration/registration.component';
 import { LoginComponent } from './container/login/login.component';
 import { HomeComponent } from './container/home/home.component';
+import { LearnrxjsComponent } from './container/learnrxjs/learnrxjs.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, RegistrationComponent, LoginComponent, HomeComponent],
+  declarations: [LayoutComponent, RegistrationComponent, LoginComponent, HomeComponent, LearnrxjsComponent],
   imports: [
     CommonModule,
     LandingRoutingModule
