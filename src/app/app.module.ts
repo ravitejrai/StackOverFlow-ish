cbd0afbfc67cfcddde508deebb116c74a7244027
Bhargavi Patel <bhargavipatel259@gmail.com>
2019-09-04 09:42:19 -0400

    buysell
    Buy/Sell Branch Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyMaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MyMaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/buysell/container/searchstock/searchstock.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from "@angular/material/table";
+import { SearchstockService, Stock } from "./searchstock.service";
+
+@Component({
+  selector: 'app-searchstock',
+  templateUrl: './searchstock.component.html',
+  styleUrls: ['./searchstock.component.scss']
+})
+export class SearchstockComponent implements OnInit {
+
+  stockItems: Stock[] = [];
+  displayedColumns: any
+  dataSource: any 
+  constructor(private StockList: SearchstockService) {}
+
+  ngOnInit() {
+    this.StockList.getDisplayStocks().subscribe(response => {
+      this.stockItems = response;
+      console.log(response);
+      this.displayedColumns = ["id", "name", "age"];
+      this.dataSource = new MatTableDataSource(this.stockItems);
+    });
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+}
+}
