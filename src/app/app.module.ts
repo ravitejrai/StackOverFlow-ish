8c829d362c1a7b79b6e083b5bbde9ce55f3f07aa
Bhargavi Patel <bhargavipatel259@gmail.com>
2019-09-05 09:48:30 -0400

    commit http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyMaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -17,12 +18,7 @@ import { MyMaterialModule } from './material.module';
     BrowserAnimationsModule,
     HttpClientModule,
     MyMaterialModule,
-<<<<<<< HEAD
   ],
-=======
-    HttpClientModule
-    ],
->>>>>>> origin/master
   providers: [],
   bootstrap: [AppComponent]
 })
