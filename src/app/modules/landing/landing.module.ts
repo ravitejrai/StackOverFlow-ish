82fcfb526fc9f925f3562864ad73f7a2e2016780
kajal <kudani@nyit.edu>
2019-09-05 17:02:32 -0400

    commit

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -9,6 +9,10 @@ import { LoginComponent } from './container/login/login.component';
 import { HomeComponent } from './container/home/home.component';
 import { MyMaterialModule } from 'src/app/material.module';
 import { LearnrxjsComponent } from './container/learnrxjs/learnrxjs.component';
+import { MatTooltipModule} from '@angular/material/tooltip';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @NgModule({
   declarations: [LayoutComponent, RegistrationComponent, LoginComponent, HomeComponent, LearnrxjsComponent],
@@ -18,7 +22,10 @@ import { LearnrxjsComponent } from './container/learnrxjs/learnrxjs.component';
     MyMaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatDatepickerModule
   ]
 })
 export class LandingModule { }
