1524e7ffa44c844b6647b06d290c15290cc67880
mandar <mandargurav445@gmail.com>
2019-09-06 13:13:35 -0400

    Update Form Save

--- src/app/modules/portfolio/container/portfolio-auth-service.service.ts
+++ src/app/modules/portfolio/container/portfolio-auth-service.service.ts
@@ -1,21 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { MessageService } from 'src/app/message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioAuthServiceService {
+  email = '';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private user:MessageService) {
+    this.user.getMessage().subscribe((data) => {
+      this.email = data.tabledata.email;
+    });
+   }
 
-  getUserDetails():Observable<User[]>{ 
-    return this.http.get<User[]>('http://localhost:3000/users')
-  }
 
-  getStockDetails():Observable<Stock[]> {
-    return this.http.get<Stock[]>('http://localhost:3000/orders')
-  }
+
+  updateUserDetails(password, firstname, lastname, phonenumber, ssn ){
+    //   const httpOptions = {
+    //   headers : new HttpHeaders({
+    //     'Content-Type': 'application/json',
+    //     'Authorization' : 'my-auth-token'
+    //   })
+    // };
+      const putData = { 
+        password:password,
+        firstname: firstname,
+        lastname: lastname,
+        phonenumber: phonenumber,
+        ssn: ssn
+      };
+      return this.http.put(`http://localhost:3000/users?email=${this.email}`,putData)
+    }
+
+    getStockDetails():Observable<Stock[]> {
+      //const email = "jsmith@virtusa.com"
+      return this.http.get<Stock[]>(`http://localhost:3000/orders?email=${this.email}`)
+    }
 }
 
 
