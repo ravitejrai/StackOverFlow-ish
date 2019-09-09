8814d755827e2eea280f507ea4a6a6c6a35b8a6a
priyareddieK <54602492+priyareddieK@users.noreply.github.com>
2019-09-09 18:16:10 -0400

    authservice spec coverage

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -20,6 +20,53 @@ imports: [HttpClientTestingModule]
     expect(service.getDetails()).toBeTruthy();
   }));
 
+it('testing http get for registration details', inject([HttpTestingController,AuthService],(httpMock: HttpTestingController, service: AuthService)=>{
+    const mockData = [
+      {
+        "email": "manogna.madineni6@gmail.com",
+        "password": "Passwoerrcdf12",
+        "firstName": "arg",
+        "lastName": "madineni",
+        "phonenumber": "5182568300",
+        "ssn": "232-43-24322",
+        "creditCardNumber": "4324-3543-5465-476568",
+        "date": "09-2019",
+        "cvv": "234334",
+        "amount": 123,
+        "id": 1
+      },
+      {
+        "email": "xzcvxcv@gmail.com",
+        "password": "Password@123",
+        "firstName": "zcvcxz",
+        "lastName": "xvcxb",
+        "phonenumber": "432-353-44567",
+        "ssn": "214-23-54357",
+        "creditCardNumber": "3223-5656-4565-46656",
+        "date": "09-2019",
+        "cvv": "23244",
+        "amount": 34535,
+        "id": 2
+      }
+    ];
+    
+    service.getDetails().subscribe(mockData => {
+     
+      expect(mockData[1].email).toBe("xzcvxcv@gmail.com");
+
+    });
+// We set the expectations for the HttpClient mock
+const req = httpMock.expectOne('http://localhost:3000/users');
+expect(req.request.method).toEqual('POST');
+
+// Then we set the fake data to be returned by the mock
+req.flush(mockData);
+
+  })
+  
+  );
+  
+
   it('testing http get for registration', inject([HttpTestingController,AuthService],(httpMock: HttpTestingController, service: AuthService)=>{
     const mockData = [
       {
